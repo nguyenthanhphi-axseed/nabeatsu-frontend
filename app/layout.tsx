30665015898b970d0bb3a111cef195382b3e439b
Nguyen Thanh Phi <nguyen_thanh_phi@axseed.co.jp>
2026-02-04 18:34:45 +0900

    update Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import { LiffProvider } from "@/components/LiffProvider";
+import { Inter } from 'next/font/google';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nabeatsu Game",
@@ -25,9 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
         <LiffProvider liffId={process.env.NEXT_PUBLIC_LIFF_ID || ""}>
           {children}
         </LiffProvider>
